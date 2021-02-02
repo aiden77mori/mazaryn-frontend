2f826bf7b0fbeffab885b01f1f9f322222dd4a92
Teddy <tedburg5@gmail.com>
2021-02-02 13:59:14 +0300

    notification panel

--- src/components/left pane/LeftPane.js
+++ src/components/left pane/LeftPane.js
@@ -1,10 +1,12 @@
 import './LeftPane.css';
 import React from 'react';
+import Notifications from './notifications/Notifications'
 
 function LeftPane(){
     return(
         <div className='leftPane'>
             <p>Left pane</p>
+            <Notifications />
         </div>
     );
 }
---
+++ src/components/left pane/notifications/Notifications.js
@@ -0,0 +1,28 @@
+import './Notifications.css';
+import React from 'react';
+
+function Notifications(){
+
+    let notifications = [
+        {
+            'name': 'Teddy',
+            'text': 'hey there'
+        },
+        {
+            'name': 'Arvand',
+            'text': 'Blockchain tech'
+        },
+        {
+            'name': 'Shifa',
+            'text': 'flutter all day'
+        }
+    ];
+
+    return(
+        <div className='notifications'>
+            
+        </div>
+    );
+}
+
+export default Notifications;
