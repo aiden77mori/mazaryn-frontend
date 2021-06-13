0ebf1de5ebfed533ba560d4e974d4c23156854eb
Teddy <tedburg5@gmail.com>
2021-06-14 01:01:31 +0300

    assigning the profile image url to a variable

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import ProfileImg from '../../lib/assets/img/profile.svg'
 
 const Header = () => {
+  
+  let profileImage = process.env.PUBLIC_URL + '/assets/images/profile.svg';
+
   return (
     <div className="border-b border-gray-200 py-6 px-32 capitalize flex justify-between items-center">
       <a href="/" className="font-semibold text-2xl">mazaryn social</a>
       <h4 className="font-semibold text-lg">Home</h4>
       <div className="flex justify-between items-center">
-        <img src={ProfileImg} alt="profile-img" />
+        <img src={profileImage} alt="profile-img" />
         <h5 className="pl-8">Teddy</h5>
       </div>
     </div>
