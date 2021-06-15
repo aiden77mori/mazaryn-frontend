9a741cc223cbedfc8f51f56454f596448969a69c
Teddy <tedburg5@gmail.com>
2021-06-15 10:19:39 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,16 +2,23 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './modules/Home';
+import SignUp from './components/security/signup/signUp';
 
 function App() {
 
   return (
     <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Home}></Route>
-        </Switch>
-      </Router>
+      <SignUp />
+
+      {
+      /*
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home}></Route>
+          </Switch>
+        </Router>
+      */
+      }
     </div>
   );
 }
---
+++ src/components/security/signup/signUp.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './signUp.css'
+
+function signUp(){
+    return(
+        <div className='sign-up-container'>
+            <form>
+                <div className='input'>
+                    <label for="email">Email</label>
+                    <input type="email" name="email" id="email" placeholder="example@gmail.com"/>
+                </div>
+                <div className='input'>
+                    <label for="password">Password</label>
+                    <input type="password" name="password" id="password" placeholder="******" />
+                </div>
+                <div className='input'>
+                    <label for="confirm-password">Confirm password</label>
+                    <input type="password" name="confirm-password" id="confirm-password" placeholder="******" />
+                </div>
+            </form>
+            <button class="sign-up-btn">Sign up</button>
+            <div class="sign-in">
+                <a href="#">Sign in</a>
+            </div>
+        </div>
+    );
+}
+
+export default signUp;
