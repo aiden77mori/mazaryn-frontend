7ddb680c8afe08d39ce23b3c7b264c089d4660d8
Teddy <tedburg5@gmail.com>
2021-06-28 19:42:23 +0300

    nav buttons

--- src/components/right pane/RightPane.js
+++ src/components/right pane/RightPane.js
@@ -4,7 +4,15 @@ import React from 'react';
 function RightPane(){
     return(
         <div className='rightPane'>
-            <button className='postButton'>Post btn</button>
+            <button className='postButton'>Post</button>
+            <nav>
+                <ul>
+                    <li>home</li>
+                    <li>bell</li>
+                    <li>wallet</li>
+                    <li>profile</li>
+                </ul>
+            </nav>
         </div>
     );
 }
