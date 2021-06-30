a22edbdc55c361eaf64a59fcceda35feaad7626d
Teddy <tedburg5@gmail.com>
2021-06-30 18:10:19 +0300

    useState hook error

--- src/components/security/signin/signIn.js
+++ src/components/security/signin/signIn.js
@@ -3,26 +3,47 @@ import './signIn.css'
 
 function SignIn(){
 
-    const [credentials, setCredentials] = useState({
-        email: 'example@gmail.com',
-        password: '****'
-    });
+    /*
+    *using an object in the useState function results in an error 
+    *"A component is changing an uncontrolled input of type text to be controlled" while changing 
+    *the input value
+    *
+    * const [credentials, setCredentials] = useState({
+    *   email: '',
+    *   password: ''
+    * });
+    * 
+    * 
+    */
+
+    const [email, setEmail] = useState('example@gmail.com');
+    const [password, setPassword] = useState('pass');
+
+    function handleChange(e){
+        if(e.target.type === 'email'){
+            setEmail(e.target.value);
+        }else{
+            setPassword
+        }
+    }
 
     return(
         <div className='sign-in-container'>
             <h2>Sign in to Mazaryn</h2>
             <form>
                 <div className='input'>
-                    <label for="email">Email</label>
-                    <input type="email" name="email" id="email" value={credentials.email}/>
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" id="email" value={email} onChange={handleChange}/>
                 </div>
                 <div className='input'>
-                    <label for="password">Password</label>
-                    <input type="password" name="password" id="password" value={credentials.password} />
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" id="password" value={password} onChange={handleChange}/>
                 </div>
             </form>
-            <button className="sign-in-btn">Sign in</button>
-            <div class="sign-in">
+            <button className="sign-in-btn" type="submit">
+                Sign in
+            </button>
+            <div className="sign-in">
                 <a href="#">Sign up</a>
             </div>
         </div>
--- src/components/security/signup/signUp.js
+++ src/components/security/signup/signUp.js
@@ -14,20 +14,20 @@ function SignUp(){
             <h2>Sign up to Mazaryn</h2>
             <form>
                 <div className='input'>
-                    <label for="email">Email</label>
+                    <label htmlFor="email">Email</label>
                     <input type="email" name="email" id="email" value={credentials.email}/>
                 </div>
                 <div className='input'>
-                    <label for="password">Password</label>
+                    <label htmlFor="password">Password</label>
                     <input type="password" name="password" id="password" value={credentials.password} />
                 </div>
                 <div className='input'>
-                    <label for="confirm-password">Confirm password</label>
+                    <label htmlFor="confirm-password">Confirm password</label>
                     <input type="password" name="confirm-password" id="confirm-password" value={credentials.confirmPassword} />
                 </div>
             </form>
-            <button class="sign-up-btn">Sign up</button>
-            <div class="sign-in">
+            <button className="sign-up-btn">Sign up</button>
+            <div className="sign-in">
                 <a href="#">Sign in</a>
             </div>
         </div>
