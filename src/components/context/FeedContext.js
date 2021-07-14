5a0be7c641558bf402dfa6b65b85089419558823
Teddy <tedburg5@gmail.com>
2021-07-14 15:46:04 +0300

    more css

--- src/components/context/FeedContext.js
+++ src/components/context/FeedContext.js
@@ -8,12 +8,22 @@ export function FeedProvider({children}){
         {
             profileImage: process.env.PUBLIC_URL + '/assets/images/profilePhoto.jpg',
             name: 'Name',
-            newText: 'lorem ipsum'
+            newText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sit amet varius ante. Quisque tortor sem, tempor bibendum justo vel, tristique blandit augue...'
         }, 
         {
             profileImage: process.env.PUBLIC_URL + '/assets/images/profilePhoto.jpg',
             name: 'Name',
-            newText: 'lorem ipsum'
+            newText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sit amet varius ante. Quisque tortor sem, tempor bibendum justo vel, tristique blandit augue...'
+        }, 
+        {
+            profileImage: process.env.PUBLIC_URL + '/assets/images/profilePhoto.jpg',
+            name: 'Name',
+            newText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sit amet varius ante. Quisque tortor sem, tempor bibendum justo vel, tristique blandit augue...'
+        }, 
+        {
+            profileImage: process.env.PUBLIC_URL + '/assets/images/profilePhoto.jpg',
+            name: 'Name',
+            newText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sit amet varius ante. Quisque tortor sem, tempor bibendum justo vel, tristique blandit augue...'
         }
     ]);
 
