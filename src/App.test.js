bdb40fe3844edd176e02eb0a1278a77e8030023f
Teddy <tedburg5@gmail.com>
2021-10-19 02:10:18 -0700

    wrapping the test

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 test('renders App component', () => {
-  render(<App />);
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
 });
