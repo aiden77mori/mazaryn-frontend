b5c84b7587c5f7e51c336a4dda2aea73725e6f00
Teddy <tedburg5@gmail.com>
2021-10-19 02:15:01 -0700

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import store from '../src/redux/store';
 
 test('renders App component', () => {
   render(
