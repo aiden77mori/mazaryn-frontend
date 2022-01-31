5ea97841d9752d6b80f0dd81a9abf737c81dcb8d
tbm5k <tedburg5@gmail.com>
2022-01-31 12:09:45 +0300

    revamped the right panel but put on hold to wait for the icon source

--- src/components/left pane/LeftPane.js
+++ src/components/left pane/LeftPane.js
@@ -1,12 +1,10 @@
-import './LeftPane.css';
+//import './LeftPane.css';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import HomeIcon from '@material-ui/icons/Home';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import AccountBalanceWalletIcon from '@material-ui/icons/AccountBalanceWallet';
-import PersonIcon from '@material-ui/icons/Person';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
-import PostCard from './PostCard/PostCard';
 
 function LeftPane(props){
     const logout = () => {
@@ -15,32 +13,30 @@ function LeftPane(props){
       }
     
       return (
-        <div className='rightPane pt-19'>
-          <PostCard />
-          <nav>
-            <ul className="rightPaneUl">
-              <li>
-                <Link to='/'>
+        <div className=' pt-19 w-1/4 flex items-center'>
+          <nav className=' h-1/2 w-full flex justify-end'>
+            <ul className=" h-full w-3/5">
+              <li className=''>
+                <Link to='/' className=' flex flex-row text-black'>
                   <HomeIcon />
+                  <p>Home</p>
                 </Link>
               </li>
               <li>
-                <Link to='/notifications'>
-                  <NotificationsIcon />
-                </Link>
-              </li>
-              <li>
-                <Link to='/wallet'>
+                <Link to='/wallet' className=' flex flex-row text-black'>
                   <AccountBalanceWalletIcon />
+                  <p>Wallet</p>
                 </Link>
               </li>
               <li>
-                <Link to='/profile'>
-                  <PersonIcon />
+                <Link to='/notifications' className=' flex flex-row text-black'>
+                  <NotificationsIcon />
+                  <p>Notifications</p>
                 </Link>
               </li>
-              <li onClick={logout} className="cursor-pointer">
+              <li onClick={logout} className="cursor-pointer flex flex-row">
                 <ExitToAppIcon />
+                <p>Exit</p>
               </li>
             </ul>
           </nav>
