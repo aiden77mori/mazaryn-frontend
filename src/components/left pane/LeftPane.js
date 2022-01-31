5fa100819e95d81cc3815d8a64c878fa3b19e00a
tbm5k <tedburg5@gmail.com>
2022-01-31 23:43:03 +0300

    installed and implemented remix icons

--- src/components/left pane/LeftPane.js
+++ src/components/left pane/LeftPane.js
@@ -1,10 +1,7 @@
 //import './LeftPane.css';
 import React from 'react';
 import { Link } from 'react-router-dom';
-import HomeIcon from '@material-ui/icons/Home';
-import NotificationsIcon from '@material-ui/icons/Notifications';
-import AccountBalanceWalletIcon from '@material-ui/icons/AccountBalanceWallet';
-import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import 'remixicon/fonts/remixicon.css'
 
 function LeftPane(props){
     const logout = () => {
@@ -17,27 +14,35 @@ function LeftPane(props){
           <nav className=' h-1/2 w-full flex justify-end'>
             <ul className=" h-full w-3/5">
               <li className=''>
-                <Link to='/' className=' flex flex-row text-black'>
-                  <HomeIcon />
-                  <p>Home</p>
+                <Link to='/' className=' flex flex-row items-center text-black focus:bg-greenborder focus:text-white rounded-bl-full rounded-tl-full h-11 pl-5'>
+                  <i class="ri-home-3-line pr-3"></i>
+                  Home
                 </Link>
               </li>
               <li>
-                <Link to='/wallet' className=' flex flex-row text-black'>
-                  <AccountBalanceWalletIcon />
-                  <p>Wallet</p>
+                <Link to='/wallet' className=' flex flex-row items-center text-black focus:bg-greenborder focus:text-white rounded-bl-full rounded-tl-full h-11 pl-5'>
+                  <i class="ri-wallet-3-line pr-3"></i>
+                  Wallet
                 </Link>
               </li>
               <li>
-                <Link to='/notifications' className=' flex flex-row text-black'>
-                  <NotificationsIcon />
-                  <p>Notifications</p>
+                <Link to='/notifications' className=' flex flex-row items-center text-black focus:bg-greenborder focus:text-white rounded-bl-full rounded-tl-full h-11 pl-5'>
+                  <i class="ri-notification-2-line pr-3"></i>
+                  Notifications
                 </Link>
               </li>
+              <li>
+                <Link className=' flex flex-row items-center text-black focus:bg-greenborder focus:text-white rounded-bl-full rounded-tl-full h-11 pl-5'>
+                  <i className="ri-message-3-line pr-3"></i>
+                  Message
+                </Link>
+              </li>
+              {/* 
               <li onClick={logout} className="cursor-pointer flex flex-row">
                 <ExitToAppIcon />
                 <p>Exit</p>
-              </li>
+              </li> 
+              */}
             </ul>
           </nav>
         </div>
