62e99af1a3c6bdea29faac4c71f9cdd0cbfe4d43
tbm5k <tedburg5@gmail.com>
2022-02-01 21:37:54 +0300

    fixed the return value of the reducer

--- src/redux/reducers/reducer_make_post.js
+++ src/redux/reducers/reducer_make_post.js
@@ -3,7 +3,7 @@ import { MAKE_POST } from "../types";
 export default makePostReducer = (state = [], action) => {
     switch(action.type){
       case `${MAKE_POST}`:
-        return {...state.push, res}
+        return {...state, createdPost: action.payload}
       default:
         return state;
     }
