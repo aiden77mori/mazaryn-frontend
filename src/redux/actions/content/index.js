97594b595cead8c3d9319619957aae4bbf56bbbf
tbm5k <tedburg5@gmail.com>
2022-02-01 21:39:58 +0300

    fixed a bug

--- src/redux/actions/content/index.js
+++ src/redux/actions/content/index.js
@@ -1,13 +1,13 @@
 import { MAKE_POST } from "../../types"
 
-export const makePost = data = async dispatch => {
+export const makePost = data => async dispatch => {
     const url = `${process.env.REACT_APP_BASE_URL}posts/`
     try{
         const res = fetch( url, {
             method: 'POST',
             body: JSON.stringify(data)
         })
-        if(res.status == 200){
+        if(res.status === 200){
             dispatch({type: MAKE_POST, payload: 'successful'})
         }
     }catch(err){
